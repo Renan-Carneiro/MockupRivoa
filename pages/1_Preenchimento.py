c811068106c27cb3de5f77e54ef599e62de96b1c
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-07 20:55:38 -0300

    Update 1_Preenchimento.py

--- pages/1_Preenchimento.py
+++ pages/1_Preenchimento.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write("<center>Insira abaixo os dados.</center>", unsafe_allow_html=True )
 formulario="""
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBZoiGYHM5PL6rxBkHfX6AuFzFdccv1F9iruUpeMaHGYLDFA/viewform?embedded=true" width="100%" height="820" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        <iframe src="https://form.jotform.com/233385865863067>
         """
 
 st.markdown(formulario, unsafe_allow_html=True)

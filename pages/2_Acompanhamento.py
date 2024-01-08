451c34e6da83357671b594fe025b27b225ac8da7
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-07 21:26:41 -0300

    Update 2_Acompanhamento.py

--- pages/2_Acompanhamento.py
+++ pages/2_Acompanhamento.py
@@ -5,7 +5,7 @@ def embed_jotform_alternative():
     <div class="jotform-embed" style="min-height: 1px;" data-id="23339603950805806" data-type="interactive" data-iframesource="www.jotform.com"></div>
     <script>(function(doc, id){var scripts=doc.getElementsByTagName("script")[0]; if (!doc.getElementById(id)){var script=doc.createElement("script"); script.async=1; script.id=id; script.src="https://cdn.jotfor.ms/s/umd/latest/for-report-embed.js"; scripts.parentNode.insertBefore(script, scripts);}})(document, "jotform-async");</script>
     """
-    st.components.v1.html(html_code, height=1000)
+    st.components.v1.html(html_code, height=800)
 
 # Example usage
 st.title("Acompanhamento das respostas")

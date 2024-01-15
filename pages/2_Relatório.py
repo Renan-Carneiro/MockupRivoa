3d4978ed29e134fa60f59ffb74c61bab6e5ed22b
Renan Carneiro <renan.moraes@engenharia.ufjf.br>
2024-01-15 16:15:26 -0300

    Credenciais
    adicionado credenciais para acessar o relatório

--- pages/2_Relatório.py
+++ pages/2_Relatório.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+def authenticate(password):
+    # Hardcoded password for demonstration purposes
+    return password == "flebografia"
 def embed_jotform_alternative():
     html_code = """
     <div class="jotform-embed" style="min-height: 1px;" data-id="23339603950805806" data-type="interactive" data-iframesource="www.jotform.com"></div>
@@ -7,6 +10,16 @@ def embed_jotform_alternative():
     """
     st.components.v1.html(html_code, height=1500)
 
+def show_sensitive_page():
+    st.title("Relatório")
+
+    password = st.text_input("Enter Password:", type="password")
+
+    if not authenticate(password):
+        st.error("Authentication failed. Access denied.")
+    else:
+        embed_jotform_alternative()
+
 # Example usage
 st.title("Relatório")
-embed_jotform_alternative()
+show_sensitive_page()
